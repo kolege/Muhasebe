0bc4a3e5168f3d86cfd49b17a1466f85fd5da636
Kolege <a.kolege@gmail.com>
2017-09-18 17:30:14 +0300

    Last

--- Muhasebe/Properties/AssemblyInfo.cs
+++ Muhasebe/Properties/AssemblyInfo.cs
@@ -6,18 +6,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Muhasebe")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Stok Muhasebesi Programı")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("cKs Yazılım")]
 [assembly: AssemblyProduct("Muhasebe")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Abdullah Kölege")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c1938058-cce9-4215-a46a-527493a02c94")]
